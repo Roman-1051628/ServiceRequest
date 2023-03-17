6927b87588b5d6ac846ddf6601e43185074b0a3b
Roman-1051628 <1051628@hr.nl>
2023-03-17 15:59:19 +0100

    changed ips

--- main.js
+++ main.js
@@ -14,29 +14,11 @@ async function getapi(url) {
     ip = data.replace(/\./g,''); 
     ip = ip.replace(/\"/g,'');
     console.log(ip);
-    if(ip === "1958517558"){
+    if(ip === "x"){
         console.log("succes");
         document.getElementById("fullName").value = "Roman Fraikin";
-        document.getElementById("nummer").value = "0636319142";
+        document.getElementById("nummer").value = "06123456789E";
         document.getElementById("company").value = "dEP";
-    }
-    if(ip === "84241203196"){
-        console.log("succes2");
-        document.getElementById("fullName").value = "Roman Telefoon";
-        document.getElementById("nummer").value = "068438922";
-        document.getElementById("company").value = "dEP sfeerontwerpers";
-    }
-    if(ip === "8424745126"){
-        console.log("succes");
-        document.getElementById("fullName").value = "Roman Thuis";
-        document.getElementById("nummer").value = "0182580509";
-        document.getElementById("company").value = "Roman Corp.";
-    }
-    if(ip === "1958517561"){
-        console.log("succes");
-        document.getElementById("fullName").value = "Dep open WIFI";
-        document.getElementById("nummer").value = "0611122233";
-        document.getElementById("company").value = "DEP Corp.";
     }
     }
 getapi(api_url);
